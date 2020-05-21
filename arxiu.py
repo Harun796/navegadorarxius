fd7414cfeb3942684c91c2e09f4585c406eb0f0d
Harun796 <harun.meraga@iesjoandaustria.org>
2020-05-21 09:01:12 +0000

    prova 2

--- arxiu.py
+++ arxiu.py
@@ -1,5 +1,7 @@
 import os, sys
 import shutil
+from shutil import copyfile
+
 menu = "0"
 while(menu!="2"):
   menu= input("1 Escull fitxers/ 2 Tancar")
@@ -11,25 +13,17 @@ while(menu!="2"):
     for file in dirs:
       print(file)
     resposta= input("Quin document vols")
-    document = open(path+"/"+resposta,"r")
-    contingut = document.read()
-    print(contingut)
-    if(contingut[0] ==" moure"):
-      if os.path.exists("demofile.txt"):
-        os.rename("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
-        shutil.move("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
-        os.replace("path/to/current/file.foo", "path/to/new/destination/for/file.foo")
-      else:
-        print("The file does not exist")
-    elif(contingut[0] == "borra"):
-      if os.path.exists("demofile.txt"):
-        os.remove("demofile.txt")
-      else:
-        print("The file does not exist")
-    elif(contingut == "copia"):
-      if os.path.exists("demofile.txt"):
-        from shutil import copyfile
-        copyfile(src, dst)
-      else:
-        print("The file does not exist")
-    document.close()
+    if os.path.exists(path+"/"+resposta):
+      document = open(path+"/"+resposta,"r")
+      contingut = document.read()
+      print(contingut)
+      if(contingut[0] ==" moure"):
+        shutil.move(path+"/"+contingut[1] , path+"/"+ contingut[2])
+      elif(contingut[0] == "borra"):
+        os.remove(path+"/"+document[1])
+      elif(contingut == "copia"):
+        copyfile(path+"/"+contingut[1], path+"/"+contingut[2])
+      document.close()
+    else:
+      print("The file does not exist")
+      resposta= input("Quin document vols")
